cbec4baef01fabfa443801f04a2b5cac2a90af15
Sören Wacker <s.wacker@tudelft.nl>
2026-05-06 16:10:59 +0200

    Restructure header: Logo | Nav | Breadcrumb

--- src/metaseed/_version.py
+++ src/metaseed/_version.py
@@ -3,12 +3,12 @@
 from __future__ import annotations
 
 __all__ = [
-    "__commit_id__",
     "__version__",
     "__version_tuple__",
-    "commit_id",
     "version",
     "version_tuple",
+    "__commit_id__",
+    "commit_id",
 ]
 
 version: str
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.1.dev188"
-__version_tuple__ = version_tuple = (0, 1, "dev188")
+__version__ = version = "0.1.dev200"
+__version_tuple__ = version_tuple = (0, 1, "dev200")
 
 __commit_id__ = commit_id = None
